630bf872fee5a480f2c8b453ff210a47ef76e9cf
haraldchan <katahane42@gmail.com>
2025-06-23 15:16:52 +0800

    added option: 提示词设置

--- entrypoints/options/components/option-details.tsx
+++ entrypoints/options/components/option-details.tsx
@@ -3,6 +3,7 @@ import { Dynamic } from 'solid-js/web'
 import OtaOptions from './ota-options'
 import ModelOptions from './model-options'
 import { Options } from '../Options'
+import PromptOptions from './prompt-options'
 
 type Props = {
 	selectedOption: Accessor<Options>
@@ -12,18 +13,20 @@ type Props = {
 const optionPanels: Record<Options, ValidComponent> = {
 	'大模型设置': ModelOptions,
 	'OTA 网址配置': OtaOptions,
+	'提示词设置': PromptOptions,
 }
 
 export default function OptionDetails({ selectedOption, descriptions }: Props) {
 	const optionDescription = () => descriptions.get(selectedOption())
 
 	return (
-		<section id="option-details">
+		<section id='option-details'>
 			<h1>{selectedOption()}</h1>
 			<p>{optionDescription()}</p>
 			<div class='option-details-container'>
 				<Dynamic component={optionPanels[selectedOption()]}></Dynamic>
 			</div>
+			<div style='height: 10vh;'></div>
 		</section>
 	)
 }
