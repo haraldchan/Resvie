32fadaec887583c8bf1448b47800430e10e0acd4
haraldchan <katahane42@gmail.com>
2025-06-23 15:11:12 +0800

    updated version

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     modules: ['@wxt-dev/module-solid', '@wxt-dev/auto-icons'],
     manifest: {
         name: "Resvie",
-        version: "2.0.2",
+        version: "2.0.3",
         description: "一键获取订单信息，轻松处理预订。",
         permissions: ["tabs", "activeTab", "clipboardWrite", "scripting", "storage", "https://api.deepseek.com/*"],
         web_accessible_resources: [
@@ -18,5 +18,4 @@ export default defineConfig({
         firefoxProfile: '.wxt/firefox-profile',
         keepProfileChanges: true,
     },
-
 })
