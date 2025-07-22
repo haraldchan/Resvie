1191db517484ea0dd1f47480c4b0b9e5078620af
haraldchan <katahane42@gmail.com>
2025-07-22 12:54:36 +0800

    updated versioning with env

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'wxt'
 
-const version = import.meta.env.WXT_APP_VERSION
-
 export default defineConfig({
     modules: ['@wxt-dev/module-solid', '@wxt-dev/auto-icons'],
-    manifest: {
+    manifest: () => ({
         name: "Resvie",
-        version: version,
+        version: import.meta.env.WXT_APP_VERSION,
         description: "一键获取订单信息，轻松处理预订。",
         permissions: ["tabs", "activeTab", "clipboardWrite", "scripting", "storage", "https://api.deepseek.com/*"],
         web_accessible_resources: [
@@ -15,7 +13,7 @@ export default defineConfig({
                 resources: ['/*.png']
             }
         ],
-    },
+    }),
     webExt: {
         firefoxProfile: '.wxt/firefox-profile',
         keepProfileChanges: true,
